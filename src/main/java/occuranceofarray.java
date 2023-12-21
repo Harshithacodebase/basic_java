7e9397920c97290c93e0e4e0018718dc45c72868
Harshithacodebase <148931972+Harshithacodebase@users.noreply.github.com>
2023-12-21 06:40:51 +0530

    Add files via upload

---
+++ src/main/java/occuranceofarray.java
@@ -0,0 +1,15 @@
+public class occuranceofarray {
+    public static void main(String[] ar){
+        int[] number = {1,2,3,6,2,2,4,2,7,8,9};
+        int count = 0;
+
+        for (int i : number){
+            if(int i=0; i>1; i++){
+                System.out.println(number[i]);
+            }
+        }
+
+    }
+
+
+}
---
+++ src/main/java/searchelement.java
@@ -0,0 +1,13 @@
+public class searchelement {
+    String[] array = {"Harshi","Jaswi","Hasish","Aruna","Prasanna"};
+    String element = "Hasish";
+
+    for(String i : array){
+        if(i == element);{
+            System.out.println("Element found");
+        }else{
+            System.out.println("Element not found");
+        }
+
+    }
+}
